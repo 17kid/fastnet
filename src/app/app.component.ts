9617e95ab7f08c5170b8b128b754ba9ed153e711
17kid <97741492+17kid@users.noreply.github.com>
2023-10-13 17:21:11 -0400

    finishing up changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fastnet';
+
+  ngOnInit(): void {
+    this.setScrollVar();
+
+    window.addEventListener("scroll", this.setScrollVar);
+    window.addEventListener("resize", this.setScrollVar);
+  }
+
+  setScrollVar(){
+    const htmlElement = document.documentElement;
+    const percentfromHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+    // console.log(Math.min(percentfromHeightScrolled * 100, 100));
+    htmlElement.style.setProperty("--scroll", Math.min(percentfromHeightScrolled * 100, 100).toString());
+  }
+
 }
